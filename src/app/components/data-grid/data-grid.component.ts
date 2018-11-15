f824d0a30ecfedc368530722620ab7d58b119c4c
Pucek9 <pucek9@gmail.com>
2018-11-15 21:37:13 +0100

    updated sorting

--- src/app/components/data-grid/data-grid.component.ts
+++ src/app/components/data-grid/data-grid.component.ts
@@ -4,7 +4,7 @@ import {select, Store} from '@ngrx/store';
 import * as paramsActions from '../../store/params/params.actions';
 import {ParamsState} from '../../store/params/params.state';
 import {paramsSelector} from '../../store/params/params.reducer';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {map, tap} from 'rxjs/operators';
 
 @Component({
@@ -17,12 +17,14 @@ export class DataGridComponent implements OnInit {
   @Input()
   headElements: string[];
   @Input()
-  elements: any[];
-  @Input()
   sortElements: string[];
+  @Input()
+  elements: any[];
 
-  order = true;
+  sortDir: boolean;
+  subscriptions = new Subscription();
   params$: Observable<ParamsState> = this.store$.pipe(select(paramsSelector));
+  sortDir$: Observable<boolean> = this.params$.pipe(map(params => params.sortDir === 1));
 
   // @HostListener('input') onInput() {
   //   this.changeSortDir();
@@ -34,32 +36,28 @@ export class DataGridComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.subscriptions.add(
+      this.params$.subscribe(params => {
+        this.sortDir = params.sortDir === 1;
+      })
+    );
   }
 
   public isSortable(head: string) {
     return this.sortElements.includes(head);
   }
 
-  public isActual(head: string) {
-    return this.params$.pipe(map(params => params.sortBy === head.toLowerCase()));
+  public isActual(i: number) {
+    return this.params$.pipe(map(params => params.sortBy === this.headElements[i].toLowerCase()));
   }
 
   public changeSortDir() {
-    this.order = !this.order;
-    this.store$.dispatch(new paramsActions.SetSortDir(this.order ? 1 : -1));
+    this.sortDir = !this.sortDir;
+    this.store$.dispatch(new paramsActions.SetSortDir(this.sortDir ? 1 : -1));
   }
 
-  public sortBy(head) {
-
-    this.isActual(head).pipe(
-      // map(value => {
-      // console.log('TODO', value);
-      // if (value) {
-      //   this.changeSortDir();
-      // } else {
-      //   this.store$.dispatch(new paramsActions.SetSortBy(head.toLowerCase()));
-      // })
-    );
+  public changeSortBy(i: number) {
+    this.store$.dispatch(new paramsActions.SetSortBy(this.headElements[i].toLowerCase()));
   }
 
 
